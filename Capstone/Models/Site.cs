55e7094db7c40b5f16790fa532d5a0044bad00e1
Sean Burger <Seanb2110@gmail.com>
2020-06-18 13:27:03 -0400

    Models Again

--- Capstone/Models/Site.cs
+++ Capstone/Models/Site.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Capstone.Models
 {
-    class Campground
+    public class Site
     {
+        public int SiteId { get; set; }
+        public int CampgroundId { get; set; }
+        public int SiteNumber { get; set; }
+        public int SiteOccupency { get; set; }
+        public bool Accessible { get; set; }
+        public int RvLength { get; set; }
+        public bool Utilities { get; set; }
+
+
+        public override string ToString()
+        {
+            return SiteId.ToString().PadRight(6) + CampgroundId.ToString().PadRight(6) + SiteNumber.ToString().PadRight(6) + SiteOccupency.ToString().PadRight(6) +
+               (Accessible ? "Accessible" : "NonAccessbile").PadRight(10) + RvLength.ToString().PadRight(8) + (Utilities ? "Utility Available" : "Utility Not Available").PadRight(20);
+        }
     }
 }
